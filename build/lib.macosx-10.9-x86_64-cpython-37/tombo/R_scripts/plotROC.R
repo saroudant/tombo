92bf7a1b33ccfff5960ea11593bc40d0cefbaf37
Soufiane Mourragui <soufiane.mourragui@free.fr>
2023-01-03 14:29:48 +0100

    functions to extract signal

--- build/lib.macosx-10.9-x86_64-cpython-37/tombo/R_scripts/plotROC.R
+++
@@ -1,8 +0,0 @@
-plotROC <- function(rocDat){
-    print(ggplot(rocDat) + geom_abline(slope=1, intercept=0) +
-          geom_path(aes(x=FP, y=TP, color=Comparison)) + theme_bw() +
-          xlab('False Positive Rate') + ylab('True Positive Rate'))
-    print(ggplot(rocDat) +
-          geom_path(aes(x=TP, y=Precision, color=Comparison)) + theme_bw() +
-          xlab('Recall') + ylab('Precision'))
-}
